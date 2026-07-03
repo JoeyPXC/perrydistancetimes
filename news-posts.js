c49af19887f5fa18a681e897f23b2976d8c4dbe2
JoeyPXC <joeybeall@gmail.com>
2026-07-02 20:16:06 -0400

    Add news post for 2026 State Track Championships

--- news-posts.js
+++ news-posts.js
@@ -16,6 +16,14 @@
 
 const NEWS_POSTS = [
     {
+        id: "2026-06-07-2026-state-track-championships",
+        sport: "track",
+        date: "2026-06-07",
+        title: "2026 State Track Championships",
+        body: "The 2026 State Track Meet was a great one for Perry Track & Field. Our 2 seated athletes had outstanding meets. Ahsha DiPietro finished off her Perry career with a State Championship in the 100m, 400m, and 800m and finished 3rd in the Seated Shot Put. Christopher Good finished State Runner-Up in the 100m, 400m, and 800m and finished 3rd in the Seat Shot Put.\n\nThe girls finished 3rd overall in Division 2. Emerson Karrenbauer won the 100m dash, anchored the 4x100 to a championship with Allie Reese, Mya Patton, and Baileigh Brown, took 2nd in the 200m dash, and the same team took 4th in the 4x200.\n\nFor the boys, Jayden Wilson finished 4th in the 400m dash and anchored the 4x400 team to 6th that had Parker Offineer, Evan Spurrell, and Donte Pocock on it. Justus Detter finished 9th in the 800m. Liston Seibert finished 10th, in the pole vaul. Evan Laubacher finished 14th in the long jump. The team finished 27th in the overall standings.\n\n[Full Results](https://www.athletic.net/TrackAndField/meet/656920/results)",
+        images: []
+    },
+{
         id: "2026-07-02-site-update",
         sport: "xc",
         date: "2026-07-02",
