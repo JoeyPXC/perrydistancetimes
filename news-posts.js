f6d947bfe8f3939c653fe4e2ce78980ce22df7e3
JoeyPXC <joeybeall@gmail.com>
2026-09-10 10:37:20 -0400

    Add news posts for XC Invitational and Hoka Showcase

--- news-posts.js
+++ news-posts.js
@@ -15,6 +15,22 @@
 //   blank line = new paragraph
 
 const NEWS_POSTS = [
+        {
+        id: "2026-09-07-ashland-arrows-xc-invitational-results",
+        sport: "xc",
+        date: "2026-09-07",
+        title: "Ashland Arrows XC Invitational Results",
+        body: "The girls and boys cross country teams competed at the Ashland Arrows XC Invitational last Saturday. The girls finished 6th overall out of 17 schools. Annamae Rogers and Julia Thompson led the team with medal winning race finishes of 22nd and 24th place. They were followed in scoring by Kiley Szcakas, Brielle Thom, and Lillian Stewart.\n\nThe boys team finished 2nd overall out of 19 schools losing only to State #1 ranked Bowling Green. Parker Simonson and Noah Dostal led the way placing 6th and 10th overall. They were followed in scoring by Broderick Gore, Ryan Sexton, and Michael Norris. \n\nThey'll compete again this weekend at Walsh Jesuit in the Pat Ritchie Invitational.",
+        images: []
+    },
+    {
+        id: "2026-08-31-hoka-night-showcase-results",
+        sport: "xc",
+        date: "2026-08-31",
+        title: "Hoka Night Showcase Results",
+        body: "The cross country teams opened their season up at SPIRE Academy Saturday night at the Hoka Night Showcase. The boys varsity team finished 14th out of 53 schools with 19 teams from other states. The team was led by Parker Simonson's 44th place medal finish. The other scorers were Noah Dostal, Ryan Sexton, Andrew Ramos, and Michael Norris. \n\nThe girls finished 38th out of 51 schools. Scoring for the girls were Julia Thompson, Brielle Thom, Lillee Woods, Annamae Rogers, and Kiley Szackas\nThe teams competes again this Saturday at the Ashland Arrows Invitational.\n",
+        images: []
+    },
     {
         id: "2026-06-07-2026-state-track-championships",
         sport: "track",
